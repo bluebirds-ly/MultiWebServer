298be3cfba017849dcdc2c4d489ea792b84f1a08
XDX <1075481183@qq.com>
2020-10-25 22:47:48 +0800

    完善cgi

--- cgi-bin/calculator.py
+++ cgi-bin/calculator.py
@@ -7,15 +7,35 @@ def run(args):
         res = a + b
     elif op == "-":
         res = a - b
+    elif op == "*":
+        res = a * b
+    elif op == "/":
+        res = a / b
+    elif op == "%":
+        res = a % b
 
-    html =   "<!DOCTYPE html>\n" \
-           + "<html lang = \"zh\">\n" \
-           + "<head>\n" \
-           + '  <meta charset="utf-8">\n' \
-           + '  <title>Calculator Result</title>\n' \
-           + '</head>\n' \
-           + '<body>\n' \
-           + "  <h2> 计算结果是：%s</h2>\n" % res \
-           + "</body>\n" \
-           + "</html>\n"
+
+    html =    "<!DOCTYPE html>\n" \
+            + "<html lang=\"en\">\n" \
+            + "<head>\n" \
+            + "    <meta charset=\"UTF-8\">\n" \
+            + "    <title>Calculator</title>\n" \
+            + "    <style>\n" \
+            + "        body{\n" \
+            + "            background-image: url(\"../pics/calculator_pic.jpg\");\n" \
+            + "            background-size: cover;\n" \
+            + "        }\n" \
+            + "    </style>\n" \
+            + "<body>\n" \
+            + "<h1 style=\"font-size: 80px;text-align:center;color:black\">\n" \
+            + "        Calculator\n" \
+            + "</h1>\n" \
+            + "<form style=\"position:fixed;top:250px;left:550px;font-size:40px\">\n" \
+            + "    num1 : %d<br>\n" %a \
+            + "    op&nbsp&nbsp&nbsp&nbsp&nbsp : %s<br>\n" %op \
+            + "    num2 : %d<br>\n" %b \
+            + "    ans&nbsp&nbsp&nbsp&nbsp&nbsp: %d<br>\n" %res \
+            + "</form>\n" \
+            + "</body>\n" \
+            + "</html>\n"
     return html
--- cgi-bin/query.py
+++ cgi-bin/query.py
@@ -1,5 +1,7 @@
 import pymysql
 from pymysql.cursors import DictCursor
+
+
 def run(args):
     conn = pymysql.connect(host="localhost", port=3306, user="root", passwd="123", db="StudentInfo", charset="utf8")
     cur = conn.cursor()
@@ -7,102 +9,237 @@ def run(args):
     data = cur.fetchone()
     print("Database version : %s" % data)
 
-    StudentID = int(args["StudentID"][0])
-    StudentName = args["StudentName"][0]
-    Class = args["Class"][0]
+    if args.get("StudentID") == None:
+        StudentID = None
+    else:
+        StudentID = int(args["StudentID"][0])
+
+    if args.get("StudentName") == None:
+        StudentName = None
+    else:
+        StudentName = args["StudentName"][0]
+
+    if args.get("Class") == None:
+        Class = None
+    else:
+        Class = args["Class"][0]
+
     op = args["op"][0]
 
+    preHtml = "<!DOCTYPE html>\n" \
+              + "<html lang=\"en\">\n" \
+              + "<head>\n" \
+              + "    <meta charset=\"UTF-8\">\n" \
+              + "    <title>Query</title>\n" \
+              + "    <style>\n" \
+              + "        body{\n" \
+              + "            background-image: url(\"../pics/query_pic.jpg\");\n" \
+              + "            background-size: cover;\n" \
+              + "        }\n" \
+              + "        table{\n" \
+              + "            border-collapse: collapse;\n" \
+              + "            text-align: center;\n" \
+              + "        }\n" \
+              + "        .td{\n" \
+              + "            width: 250px;\n" \
+              + "        }\n" \
+              + "        h2 {\n" \
+              + "            text-align: center;\n" \
+              + "            font-size: 40px;\n" \
+              + "        }\n" \
+              + "    </style>\n" \
+              + "</head>\n" \
+              + "<body>\n" \
+              + "    <h1 style=\"font-size: 80px;text-align:center;color:black\">\n" \
+              + "        Query\n" \
+              + "    </h1>\n"
 
-    inserthtml = "<!DOCTYPE html>\n" \
-               + "<html lang = \"zh\">\n" \
-               + "<head>\n" \
-               + '  <meta charset="utf-8">\n' \
-               + '  <title>Calculator Result</title>\n' \
-               + '</head>\n' \
-               + '<body>\n' \
-               + "  <h2> 成功插入学生数据：%s！</h2>\n" % StudentID \
-               + "</body>\n" \
+    tailHtml = "</body>\n" \
                + "</html>\n"
 
-    deletehtml = "<!DOCTYPE html>\n" \
-                 + "<html lang = \"zh\">\n" \
-                 + "<head>\n" \
-                 + '  <meta charset="utf-8">\n' \
-                 + '  <title>Calculator Result</title>\n' \
-                 + '</head>\n' \
-                 + '<body>\n' \
-                 + "  <h2> 成功删除学生数据：%s！</h2>\n" % StudentID \
-                 + "</body>\n" \
-                 + "</html>\n"
+    insertsql = "INSERT INTO Student(StudentID, StudentName, Class)VALUES ( %d , '%s',  '%s' );" % (
+    StudentID, StudentName, Class)
+    deletesql = "DELETE FROM Student WHERE StudentID = %d;" % (StudentID)
+    querysql = "SELECT * FROM Student WHERE StudentID = %d;" % (StudentID)
+    showallsql = "SELECT * FROM Student;"
 
-    notfoundhtml = "<!DOCTYPE html>\n" \
-                 + "<html lang = \"zh\">\n" \
-                 + "<head>\n" \
-                 + '  <meta charset="utf-8">\n' \
-                 + '  <title>Calculator Result</title>\n' \
-                 + '</head>\n' \
-                 + '<body>\n' \
-                 + "  <h2> 学号为%s的学生不存在！</h2>\n" % StudentID \
-                 + "</body>\n" \
-                 + "</html>\n"
-    existedhtml = "<!DOCTYPE html>\n" \
-                 + "<html lang = \"zh\">\n" \
-                 + "<head>\n" \
-                 + '  <meta charset="utf-8">\n' \
-                 + '  <title>Calculator Result</title>\n' \
-                 + '</head>\n' \
-                 + '<body>\n' \
-                 + "  <h2> 学号为%s的学生已经存在了！</h2>\n" % StudentID \
-                 + "</body>\n" \
-                 + "</html>\n"
-
-    insertsql = "INSERT INTO Student(StudentID, StudentName, Class)VALUES ( %d , '%s',  '%s' );"%(StudentID, StudentName, Class)
-    deletesql = "DELETE FROM Student WHERE StudentID = %d;"%(StudentID)
-    querysql = "SELECT * FROM Student WHERE StudentID = %d;"%(StudentID)
     if op == "Insert":
-        cur.execute(querysql)      #插入前先判断是否存在
+        cur.execute(querysql)  # 插入前先判断是否存在
         results = cur.fetchall()
-        rowcount = 0;
+        rowcount = 0
         for row in results:
             rowcount += 1
             stuid = row[0]
             stuname = row[1]
             stuclass = row[2]
         if rowcount > 0:
-            html = existedhtml
-            conn.close()
-            return html
-        cur.execute(insertsql)
-        conn.commit()
-        print("insert successful")
-        html = inserthtml
+            existedhtml = "    <h2>该ID学生信息已经存在！信息如下</h2>\n" \
+                          + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                          + "        <table align=\"center\" border=\"1px\">\n" \
+                          + "            <tr>\n" \
+                          + "                <th>Student ID</th>\n" \
+                          + "                <th>Student Name</th>\n" \
+                          + "                <th>Class</th>\n" \
+                          + "            </tr>\n" \
+                          + "            <tr>\n" \
+                          + "                <td class=\"td\">%d</td>\n" % stuid \
+                          + "                <td class=\"td\">%s</td>\n" % stuname \
+                          + "                <td class=\"td\">%s</td>\n" % stuclass \
+                          + "            </tr>\n" \
+                          + "        </table>\n" \
+                          + "    </form>\n"
+            html = preHtml + existedhtml + tailHtml
+        else:
+            cur.execute(insertsql)
+            conn.commit()
+            inserthtml = "    <h2>学生信息插入成功！信息如下</h2>\n" \
+                         + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                         + "        <table align=\"center\" border=\"1px\">\n" \
+                         + "            <tr>\n" \
+                         + "                <th>Student ID</th>\n" \
+                         + "                <th>Student Name</th>\n" \
+                         + "                <th>Class</th>\n" \
+                         + "            </tr>\n" \
+                         + "            <tr>\n" \
+                         + "                <td class=\"td\">%d</td>\n" % StudentID \
+                         + "                <td class=\"td\">%s</td>\n" % StudentName \
+                         + "                <td class=\"td\">%s</td>\n" % Class \
+                         + "            </tr>\n" \
+                         + "        </table>\n" \
+                         + "    </form>\n"
+            html = preHtml + inserthtml + tailHtml
     elif op == "Delete":
-        cur.execute(deletesql)
-        conn.commit()
-        html = deletehtml
-        print("delete successful")
-    elif op == "Query":
-        cur.execute(querysql)
-        results = cur.fetchall()  #获取全部结果
-        rowcount = 0;
+        cur.execute(querysql)  # 删除前先判断是否存在
+        results = cur.fetchall()
+        rowcount = 0
         for row in results:
             rowcount += 1
             stuid = row[0]
             stuname = row[1]
             stuclass = row[2]
-        if rowcount == 0:       #如果有结果
-            html = notfoundhtml
-        else :
-            html = "<!DOCTYPE html>\n" \
-                 + "<html lang = \"zh\">\n" \
-                 + "<head>\n" \
-                 + '  <meta charset="utf-8">\n' \
-                 + '  <title>Calculator Result</title>\n' \
-                 + '</head>\n' \
-                 + '<body>\n' \
-                 + "  <h2> 查询到的学生数据为：%d %s %s！</h2>\n" % (stuid, stuname, stuclass) \
-                 + "</body>\n" \
-                 + "</html>\n"
+        if rowcount == 0:
+            notexistedhtml = "    <h2>该ID学生信息不存在！</h2>\n" \
+                             + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                             + "        <table align=\"center\" border=\"1px\">\n" \
+                             + "            <tr>\n" \
+                             + "                <th>Student ID</th>\n" \
+                             + "                <th>Student Name</th>\n" \
+                             + "                <th>Class</th>\n" \
+                             + "            </tr>\n" \
+                             + "            <tr>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "            </tr>\n" \
+                             + "        </table>\n" \
+                             + "    </form>\n"
+            html = preHtml + notexistedhtml + tailHtml
+        else:
+            cur.execute(deletesql)
+            conn.commit()
+            deletehtml = "    <h2>学生信息删除成功！信息如下</h2>\n" \
+                         + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                         + "        <table align=\"center\" border=\"1px\">\n" \
+                         + "            <tr>\n" \
+                         + "                <th>Student ID</th>\n" \
+                         + "                <th>Student Name</th>\n" \
+                         + "                <th>Class</th>\n" \
+                         + "            </tr>\n" \
+                         + "            <tr>\n" \
+                         + "                <td class=\"td\">%d</td>\n" % stuid \
+                         + "                <td class=\"td\">%s</td>\n" % stuname \
+                         + "                <td class=\"td\">%s</td>\n" % stuclass \
+                         + "            </tr>\n" \
+                         + "        </table>\n" \
+                         + "    </form>\n"
+            html = preHtml + deletehtml + tailHtml
+    elif op == "Query":
+        cur.execute(querysql)  # 查询判断是否存在
+        results = cur.fetchall()
+        rowcount = 0
+        for row in results:
+            rowcount += 1
+            stuid = row[0]
+            stuname = row[1]
+            stuclass = row[2]
+        if rowcount == 0:
+            notexistedhtml = "    <h2>该ID学生信息不存在！</h2>\n" \
+                             + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                             + "        <table align=\"center\" border=\"1px\">\n" \
+                             + "            <tr>\n" \
+                             + "                <th>Student ID</th>\n" \
+                             + "                <th>Student Name</th>\n" \
+                             + "                <th>Class</th>\n" \
+                             + "            </tr>\n" \
+                             + "            <tr>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "            </tr>\n" \
+                             + "        </table>\n" \
+                             + "    </form>\n"
+            html = preHtml + notexistedhtml + tailHtml
+        else:
+            queryhtml = "    <h2>学生信息查询成功！信息如下</h2>\n" \
+                        + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                        + "        <table align=\"center\" border=\"1px\">\n" \
+                        + "            <tr>\n" \
+                        + "                <th>Student ID</th>\n" \
+                        + "                <th>Student Name</th>\n" \
+                        + "                <th>Class</th>\n" \
+                        + "            </tr>\n" \
+                        + "            <tr>\n" \
+                        + "                <td class=\"td\">%d</td>\n" % stuid \
+                        + "                <td class=\"td\">%s</td>\n" % stuname \
+                        + "                <td class=\"td\">%s</td>\n" % stuclass \
+                        + "            </tr>\n" \
+                        + "        </table>\n" \
+                        + "    </form>\n"
+            html = preHtml + queryhtml + tailHtml
+    elif op == "ShowAll":
+        # print(op)
+        cur.execute(showallsql)
+        results = cur.fetchall()  # 获取全部结果
+        rowcount = 0
+        tablehtml = "    <h2>学生信息查询成功！信息如下</h2>\n" \
+                    + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                    + "        <table align=\"center\" border=\"1px\">\n" \
+                    + "            <tr>\n" \
+                    + "                <th>Student ID</th>\n" \
+                    + "                <th>Student Name</th>\n" \
+                    + "                <th>Class</th>\n" \
+                    + "            </tr>\n"
+        for row in results:
+            rowcount += 1
+            stuid = row[0]
+            stuname = row[1]
+            stuclass = row[2]
+            tablehtml += "            <tr>\n" \
+                         + "                <td class=\"td\">%d</td>\n" % stuid \
+                         + "                <td class=\"td\">%s</td>\n" % stuname \
+                         + "                <td class=\"td\">%s</td>\n" % stuclass \
+                         + "            </tr>\n"
+        tablehtml += "        </table>\n" \
+                     + "    </form>\n"
 
+        if rowcount == 0:  # 如果有结果
+            notexistedhtml = "    <h2>无任何学生信息！</h2>\n" \
+                             + "    <form name=\"input\" style=\"font-size:40px;\">\n" \
+                             + "        <table align=\"center\" border=\"1px\">\n" \
+                             + "            <tr>\n" \
+                             + "                <th>Student ID</th>\n" \
+                             + "                <th>Student Name</th>\n" \
+                             + "                <th>Class</th>\n" \
+                             + "            </tr>\n" \
+                             + "            <tr>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "                <td class=\"td\">---</td>\n" \
+                             + "            </tr>\n" \
+                             + "        </table>\n" \
+                             + "    </form>\n"
+            html = preHtml + notexistedhtml + tailHtml
+        else:
+            html = preHtml + tablehtml + tailHtml
     conn.close()
     return html
